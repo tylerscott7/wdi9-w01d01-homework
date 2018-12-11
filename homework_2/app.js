7991d1d2eb1cc559480fbaf9480ff5f486d308b6
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2018-12-11 15:49:36 -0700

    Commit 4 - Fizz Buzz answered

--- homework_2/app.js
+++ homework_2/app.js
@@ -28,3 +28,16 @@ for (let i=0; i<20; i++){
     }
     console.log("Love me, pet me! HSSSSSS!");
 };
+
+// Problem 4:
+for (let i=1; i <= 100; i++){
+    if (i%15 == 0){
+        console.log("FizzBuzz");
+    } else if (i%5 == 0){
+        console.log("Buzz");
+    } else if (i%3 == 0){
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    };
+};
