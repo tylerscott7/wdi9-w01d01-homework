7f0716179b6b3fc002a8622ca72f088708eb1e69
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2018-12-11 15:37:25 -0700

    Commit 3 - Excited Kittens answered

--- homework_2/app.js
+++ homework_2/app.js
@@ -8,3 +8,23 @@ for (let i=2; i <= 200; i+=2){
     console.log(i);
 };
 
+// Problem 3:
+for (let i=0; i<20; i++){
+    if (i % 2 == 0){                                   // Only operates on even numbers
+        let rand = Math.ceil(Math.random() * 3);     // Returns random number 1,2, or 3
+        switch(rand) {
+            case 1:
+              console.log("...human...why you taking pictures of me?...");
+              break;
+            case 2:
+              console.log("...the catnip made me do it...");
+              break;
+            case 3:
+              console.log("...why does the red dot always get away...");
+              break;
+            default:
+              //nothing
+          }
+    }
+    console.log("Love me, pet me! HSSSSSS!");
+};
